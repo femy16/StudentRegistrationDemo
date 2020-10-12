32c2c5bd569ac1a62388ff1b2985c1091fc3bccb
Femy Stephen <femystephen16@gmail.com>
2020-10-12 18:20:55 +0100

    test2

--- Controllers/StudentRegController.cs
+++ Controllers/StudentRegController.cs
@@ -14,7 +14,7 @@ namespace StudentRegistrationDemo.Controllers
         { 
             new Student{Id = 100,Name="Geraald Nimooy",age=14},
              new Student{Id = 101,Name="Jeramy Weeler",age=16},
-              new Student{Id = 102,Name="Gerry Newlyr",age=14},
+              new Student{Id = 102,Name="Gerry Newlyr",age=15},
              new Student{Id = 10,Name="Jimy Wxlr",age=16},
         };
         // GET: api/StudentReg
